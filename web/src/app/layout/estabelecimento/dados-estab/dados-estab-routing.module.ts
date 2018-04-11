4ea7375dad4169f5a70c74154580e20fd45fd3ec
ClovesF <cloves.sjc@gmail.com>
2018-04-10 21:02:48 -0300

    Dados Estabelecimentos

---
+++ web/src/app/layout/estabelecimento/dados-estab/dados-estab-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DadosEstabComponent } from './dados-estab.component';
+
+const routes: Routes = [
+    {
+        path: '', 
+        component: DadosEstabComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class DadosEstabRoutingModule { }
