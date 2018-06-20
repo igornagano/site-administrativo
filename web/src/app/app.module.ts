213c9661add7f3f8f8f4f325fd9c4331560cfa3f
Igor Nagano <igornagano@hotmail.com>
2018-06-19 22:45:08 -0300

    Revert "Merge branch 'master' of https://github.com/igornagano/site-administrativo"
    This reverts commit d74ae1fabe5f8b8151a071314e1d6a5d92e9ef43, reversing
    changes made to 405618899a1805c9964a05e61762afcad2a39638.

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,27 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+import { EmpresaService } from './layout/service/empresa.service';
+import { EstabelecimentoService } from './layout/service/estabelecimento.service';
+import { ColaboradorService } from './layout/service/colaborador.service';
+import { VagaService } from './layout/service/vaga.service';
+import { ReservaService } from './layout/service/reserva.service';
+import { SensorService } from './layout/service/sensor.service';
+import { GestorService } from './layout/service/gestor.service';
+import {Conf} from './layout/service/conf';
 
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioComponent } from './formulario/formulario.component';
-import { NewCmpComponent } from './new-cmp/new-cmp.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormularioService } from './formulario.service'
+import { AuthGuard } from './shared';
+import { MenuComponent } from './menu/menu.component';
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+    // for development
+    // return new TranslateHttpLoader(http, '/start-angular/SB-Admin-BS4-Angular-5/master/dist/assets/i18n/', '.json');
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormularioComponent,
-    NewCmpComponent,
-    
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [
-    FormularioService
-  ],
-  bootstrap: [AppComponent]
+    imports: [
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        FormsModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoader,
+                deps: [HttpClient]
+            }
+        }),
+        AppRoutingModule
+    ],
+    declarations: [AppComponent, MenuComponent],
+    providers: [AuthGuard, EmpresaService, EstabelecimentoService, ColaboradorService, VagaService, ReservaService, SensorService, Conf,GestorService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- web/src/app/formulario/formularioController.js
+++
@@ -1,7 +0,0 @@
-angular.module('formulario', [])
-.controller('dados', function($scope,$http) {
-   	$http.get('http://localhost:8000/cliente/usuario/teste2@teste.com').
-        then(function(response) {
-            $scope.dados = response.data;
-        });
-});
